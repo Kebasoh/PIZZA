5356c75b752cfa2b3881ed97574e95da15faf4cf
Kebasoh <ongatikebaso@gmail.com>
2019-11-08 20:18:33 +0300

    added the script.js business logic

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function getSizeValue() {
+    var selectedValue = document.getElementById("size").value;
+    return parseInt(selectedValue);
+}
+function getCrust() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getToppings() {
+    var selectedToppings = document.getElementById("toppings").value;
+    return parseInt(selectedToppings);
+}
+function getQuantity() {
+    var selectedQuantity = document.getElementById("quan").value;
+    return parseInt(selectedQuantity);
+}
+function getTotalAmount() {
+    var totalAmount = (getSizeValue() + getCrust() + getToppings()) * getQuantity();
+    alert("You have Ordered" + getQuantity("")  +  " pizza."  +  ""  +  " The Total Amount is kshs "  +  (totalAmount)  +  ""  +  " Thank you for your order welcome again.");
+    alert("Continue shoppping with us each and every time .")
+}
